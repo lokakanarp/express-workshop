4596d0243faab2bbbcda4d42c239d30674e7899e
Loka Kanarp <lokakanarp@gmail.com>
2020-03-02 22:03:16 +0100

    Some restructuring and a first attemt to post to the db.

--- server.js
+++ server.js
@@ -10,75 +10,56 @@ app.use(express.static("public"));
 app.use(formidable());
 
 //Testar lite
-// var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "root",
-//   database: "cute_animals",
-//   port: 8889
-// });
-//
-// con.connect(function(err) {
-//   if (err) {console.log("error")}
-//   else {console.log("you are Connected!")}
-//
-//    con.query('SELECT animal FROM animals', function(err, results) {
-//      if(err) {console.log("det blev fel")}
-//      else {console.log(results)}
-//  });
-//
-// });
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "express_workshop_db",
+  port: 8889
+});
+
+con.connect(function(err) {
+  if (err) {console.log("error")}
+  else {console.log("you are Connected!")}
+
+});
+
+
 
 app.post('/create-post', function (req, res) {
-
-  fs.readFile(__dirname + '/data/posts.json', function (error, file) {
-    var newValue = req.fields.blogpost;
-    var blogPost = {[JSON.stringify(Date.now())] : newValue};
-    var parsedFile = JSON.parse(file);
-    var myObject = Object.assign(parsedFile, blogPost);
-    var finalObject = JSON.stringify(myObject);
-
-    fs.writeFile(__dirname + '/data/posts.json', finalObject, function (error) {
-      if (error) {console.log("an error")}
-      else {res.send(blogPost)}
-    });
-  });
+  var newValue = req.fields.blogpost;
+  con.query('INSERT INTO blogposts (blogpost) VALUES ("'+ newValue +'")', function(err, data) {
+    if(err) {console.log("det blev fel")}
+    else {
+      res.send(data);
+    }
+  })
+  // fs.readFile(__dirname + '/data/posts.json', function (error, file) {
+  //   var newValue = req.fields.blogpost;
+  //   var blogPost = {[JSON.stringify(Date.now())] : newValue};
+  //   var parsedFile = JSON.parse(file);
+  //   var myObject = Object.assign(parsedFile, blogPost);
+  //   var finalObject = JSON.stringify(myObject);
+  //
+  //   fs.writeFile(__dirname + '/data/posts.json', finalObject, function (error) {
+  //     if (error) {console.log("an error")}
+  //     else {res.send(blogPost)}
+  //   });
+  // });
 });
 
 app.get('/get-posts', function (req, res) {
   //res.sendFile(__dirname + '/data/posts.json');
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "cute_animals",
-    port: 8889
-  });
+  //var results;
+  con.query('SELECT blogpost FROM blogposts', function(err, data) {
+    if(err) {console.log("det blev fel")}
+    else {
+      //var results = data;
+      res.send(data);
 
-  con.connect(function(err) {
-    if (err) {console.log("error")}
-    else {console.log("you are Connected!")}
-
-     con.query('SELECT animal FROM animals', function(err, results) {
-       if(err) {console.log("det blev fel")}
-       else {
-         res.send(results)
-       }
-   });
-
+    }
   });
 });
-
-// app.get('/', function (req, res) {
-//
-//   con.query('SELECT * FROM animals', function(err, results) {
-//     if(err) {console.log("det blev fel")}
-//     else {res.render(results[0].animal)}
-//
-//           });
-//
-//
-// });
 
 app.listen(3000, function () {
   console.log('Hej! Server is listening on port 3000. Ready to accept requests!');
